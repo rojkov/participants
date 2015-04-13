dce3b0cb2c149a0c8e65a2d07f1cef8e0e33162a
Dmitry Rozhkov <dmitry.rozhkov@jolla.com>
2015-04-13 12:01:00 +0300

    Initial implementation done

--- participants/tasks/webhooks.py
+++ participants/tasks/webhooks.py
@@ -2,15 +2,21 @@ from __future__ import absolute_import
 
 from participants.celery import app
 
+process_def = """<?xml version="1.0"?>
+<process>
+    <action participant="participant2" />
+</process>
+"""
+
 @app.task
 def participant1(workitem):
     """Test participant1 function."""
 
-    if "counter" not in workitem.fields:
-        workitem.fields["counter"] = 0
+    if "counter" not in workitem["fields"]:
+        workitem["fields"]["counter"] = 0
     else:
-        workitem.fields["counter"] += 1
-    workitem.fields["some_process"] = process_def
+        workitem["fields"]["counter"] += 1
+    workitem["fields"]["some_process"] = process_def
 
     return workitem
 
