93eb1925d06bed6bf4c45f049adea5a717cfd1a7
Dmitry Rozhkov <dmitry.rozhkov@jolla.com>
2015-04-16 15:39:12 +0300

    Add webhook tasks

--- participants/tasks/webhooks.py
+++ participants/tasks/webhooks.py
@@ -1,5 +1,11 @@
 from __future__ import absolute_import
 
+import os
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'webhook_launcher.settings'
+
+from webhook_launcher.app.utils import handle_payload
+from webhook_launcher.app.utils import relay_payload
 from participants.celery import app
 
 process_def = """<?xml version="1.0"?>
@@ -23,4 +29,22 @@ def participant1(workitem):
 @app.task
 def participant2(workitem):
     """Test participant2 function."""
+    return workitem
+
+@app.task
+def handle_webhook(workitem):
+    """Handle POST request to a webhook."""
+
+    payload = workitem["fields"]["payload"]
+    handle_payload(payload)
+
+    return workitem
+
+@app.task
+def relay_webhook(workitem):
+    """Relay webhook POST request to task queue."""
+
+    payload = workitem["fields"]["payload"]
+    relay_payload(payload)
+
     return workitem
